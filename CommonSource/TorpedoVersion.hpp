278bc24f6645a506af528f556b60184c9058f21b
Zentrom <sokaturgen-17@hotmail.com>
2022-06-12 18:42:22 +0200

    v0.0.5<->Audio And Animations
    [FEATURES]
    -Added Ship textures.
    -Added ShipFlags to distinctiate allied and enemy ships.
    -BattleShip now has a Cannon which fires an animated projectile.
    -Players can't shoot while there is a firing animation in progress.
    -Added tracing Particles to the Projectile.
    -Player Ships now sink when destroyed.
    -Losing player's BattleShip sinks when game ends.
    -Added Game Music and distance based fire/miss sounds.
    -Added music mute button 'm'

--- CommonSource/TorpedoVersion.hpp
+++ CommonSource/TorpedoVersion.hpp
@@ -4,6 +4,6 @@
 struct TorpedoVersion {
 	const int majorVersion = 0;
 	const int betaVersion = 0;
-	const int alphaVersion = 4;
-	const char experimentalVersion = 'f';
+	const int alphaVersion = 5;
+	const char experimentalVersion = ' ';
 };
